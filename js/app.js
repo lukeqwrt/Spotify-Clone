eb3012eee4dbdeb6610f38053c52e72cda389360
luke <luke.adrian.vinluan.sillano@gmail.com>
2021-07-25 23:47:57 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+window.addEventListener('load', () => {
+    const loader =  document.querySelector('.loader');
+    loader.classList.add("hidden")
+})
+
+
+//     const olivia =  document.getElementById("oliv")
+//     const bts =  document.getElementById("bts")
+//     const bp =  document.getElementById("bp")
+//     const taylor =  document.getElementById("taylor")
+//     const ariana =  document.getElementById("ariana")
+//     const itzy =  document.getElementById("itzy")
+//     const body = document.getElementById("content")
+
+
+
+//     olivia.addEventListener("mouseover", (e) => {
+//             e.stopPropagation();
+            
+//             body.style.background = "url('/assets/change-body-bg-white-try.jpg')"
+//             body.style.transition = "background 1s"
+//         }, false)
+
+//     bts.addEventListener("mouseover", (e) => {
+//             e.stopPropagation();
+//             body.style.background = "url('/assets/bts-bg.jpg')"
+//             body.style.transition = "background 1s"
+//     }, false)
+//     bp.addEventListener("mouseover", (e) => {
+//             e.stopPropagation();
+//             body.style.background = "url('/assets/bp-bg.jpg')"
+//             body.style.transition = "background 1s"
+//     }, false)
+//     taylor.addEventListener("mouseover", (e) => {
+//             e.stopPropagation();
+//             body.style.background = "url('/assets/taylor-bg.jpg')"
+//             body.style.transition = "background 1s"
+//     }, false)
+//     ariana.addEventListener("mouseover", (e) => {
+//             e.stopPropagation();
+//             body.style.background = "url('/assets/ari-bg.jpg')"
+//             body.style.transition = "background 1s"
+//     }, false)
+//     itzy.addEventListener("mouseover", (e) => {
+//             e.stopPropagation();
+//             body.style.background = "url('/assets/itzy-bg.jpg')"
+//             body.style.transition = "background 1s"
+//     }, false)
